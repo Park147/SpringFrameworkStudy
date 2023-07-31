c281e2813e26a54187a31468b73b3ca5bba881cd
Park147 <grin4mation@gmail.com>
2023-07-31 16:42:35 +0900

    스프링프레임워크 회원가입 기능중

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -1,15 +1,21 @@
-package com.codingrecipe.project01.controller;
+package com.codingrecipe.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
-    @GetMapping("/member/save") // /member/member/save
-//    @GetMapping("/save")
+//    @GetMapping("/member/save") // /member/member/save
+    @GetMapping("/save")
     public  String saveForm() {
         return  "save";
+    }
+
+    @PostMapping("/save")
+    public String save(){
+
     }
 }
---
+++ src/main/java/com/codingrecipe/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.codingrecipe.member.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+}
